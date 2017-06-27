564e0fdfc18f81a6102ca7fbcb192fdb6586e750
Charles Emrich and Jared Eiseman <jared@>
2017-06-27 11:05:26 -0700

    finish clock app

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -1,8 +1,8 @@
 var Clock = require('./../js/clock.js').clock;
-
-
+var alert = new Audio('http://trekcore.com/audio/redalertandklaxons/tas_red_alert.mp3');
+alert.loop = true;
 $(document).ready(function() {
-  var ourClock = new Clock();
+  var ourClock = new Clock(parseInt($('input[name="snooze-length"]').val()));
 
   $('.current-time').text(ourClock.currentTime());
   setInterval(function() {
@@ -10,8 +10,7 @@ $(document).ready(function() {
 
     ourClock.checkAlarms();
     if (ourClock.alarmIsActive) {
-      console.log('alarm is sounding');
-      // ourClock.clearAlarm();
+      alert.play();
     }
 
   }, 1000);
@@ -20,13 +19,32 @@ $(document).ready(function() {
     event.preventDefault();
     var inputtedTime = $("input[name='time']").val();
     ourClock.setAlarm(inputtedTime);
+    $('#set-alarm-form').css('display', 'none');
   });
 
   $('#snooze-settings').submit(function(e) {
     e.preventDefault();
     ourClock.snoozeInterval = parseInt($('input[name="snooze-length"]').val());
+    $('#snooze-settings').css('display', 'none');
   });
+
+  $('#snooze').click(function() {
+    ourClock.snooze();
+    alert.pause();
+    alert.currentTime = 0;
+  });
+
+  $('.settings').click(function() {
+    $('#snooze-settings').css('display', 'flex');
+  });
+
   $('#dismiss-alarm').click(function() {
     ourClock.clearAlarm();
+    alert.pause();
+    alert.currentTime = 0;
+  });
+
+  $('#set-alarm').click(function() {
+    $('#set-alarm-form').css('display', 'flex');
   });
 });
--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,9 @@
 //business logic stuff for a particular module
-var Clock = function() {
+var Clock = function(snoozeInterval) {
   this.alarms = [];
   this.time = "";
   this.alarmIsActive = false;
-  this.snoozeInterval = 5;
+  this.snoozeInterval = snoozeInterval;
   this.currentActiveAlarm = null;
 };
 
@@ -17,22 +17,31 @@ Clock.prototype.setAlarm = function(time) {
   var formattedTime = time.split(":");
   var newTime = moment().hour(formattedTime[0]).minute(formattedTime[1]);
   this.alarms.push(newTime);
-}
+};
 
 Clock.prototype.checkAlarms = function() {
   var clock = this;
   this.alarms.forEach(function(alarm, index) {
     if (alarm.format('h:mm A') === clock.time.format('h:mm A')) {
-      clock.alarmIsActive = true;
-      clock.currentActiveAlarm = index;
+      clock.setAlarmState(true, index);
     }
   });
   return clock.alarmIsActive;
-}
+};
+
+Clock.prototype.snooze = function() {
+  this.alarms[this.currentActiveAlarm].add(this.snoozeInterval, 'm');
+  this.setAlarmState(false, null);
+};
 
 Clock.prototype.clearAlarm = function() {
-  this.alarmIsActive = false;
   this.alarms.splice(this.currentActiveAlarm, 1);
-}
+  this.setAlarmState(false, null);
+};
+
+Clock.prototype.setAlarmState = function(alarmIsActive, currentActiveAlarm) {
+  this.alarmIsActive = alarmIsActive;
+  this.currentActiveAlarm = currentActiveAlarm;
+};
 
 exports.clock = Clock;
